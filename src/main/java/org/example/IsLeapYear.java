6fd2821e92a72e0cd233bfa92b0260a2d7da1060
Allahverdiyeva Shafayat <allahverdiyevashafayat@outlook.com>
2023-08-05 15:57:26 +0000

    Initial commit

---
+++ src/main/java/org/example/IsLeapYear.java
@@ -0,0 +1,10 @@
+package org.example;
+
+public class IsLeapYear{
+        public static boolean isLeapYear(int year) { if ((year%400==0) || ((year % 4==0) && (year % 100!=0))){
+            return true;
+        }
+        else {return false;}
+
+        }}
+
---
+++ src/main/java/org/example/Main.java
@@ -0,0 +1,18 @@
+package org.example;
+
+public class Main {
+    public static void main(String[] args) {
+int year=2018;
+
+
+    IsLeapYear isLeapYear= new IsLeapYear();
+
+
+        if (IsLeapYear. isLeapYear(year)) {
+            System.out.println(year + "  високосный год.");
+        } else {
+            System.out.println(year + "  не високосный год.");
+        }
+
+    }
+    }
